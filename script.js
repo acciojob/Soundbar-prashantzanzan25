15c70fd98ce38db2761c8403a9959ddaa725fb39
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-22 18:11:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function(){
+	const sounds = ['sound1.mp3', 'sound2.mp3', 'sound3.mp3'];
+
+	const buttonContainer = document.getElementById('buttons');
+
+	sounds.forEach((sound, index) => {
+		const button = document.createElement('button');
+		button.classList.add('btn');
+		button.txtContent = 'sound ${index+1}';
+		button.addEventListener('click', () => playSound(sound));
+		buttonsContainer.appendChild(button);
+	});
+	
+	const stopButton = document.createElement('button');
+	stopButton.classList.add('btn', 'stop');
+	stopButton.textContent = 'stop';
+	stopButton.addEventListener('click', stopSounds);
+	buttonContainer.appendChild(stopButton);
+
+	function playSound(sound) {
+		const audio = new Audio('sounds/${sound}');
+		audio.play();
+	}
+
+	function stopSounds() {
+		const allAudio = document.querySelectorAll('audio');
+		allAudio.forEach(audio => audio.pause());
+	}
+});
